b40c9344dc95eaa361292142356ebb6ff1bac022
ibharathboga <ibharathboga@gmail.com>
2025-10-03 13:12:44 +0000

    [frontend] environment.ts, add firebaseConfig
    [frontend] wip: environment.ts: make firebaseConfig exportable

---
+++ frontend/src/environment.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDLdX1kqTRsamkZWKH-YMJFqDmIOFFaN1w",
+  authDomain: "microblog-iapp.firebaseapp.com",
+  projectId: "microblog-iapp",
+  storageBucket: "microblog-iapp.firebasestorage.app",
+  messagingSenderId: "759376736637",
+  appId: "1:759376736637:web:7b6813450d15260cdedc14"
+};
