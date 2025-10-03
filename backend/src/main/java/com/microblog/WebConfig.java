1afd07505794ec29cefeaed49842d6ba60c59f5f
ibharathboga <ibharathboga@gmail.com>
2025-10-03 05:29:13 +0000

    [backend] WebConfig, resolve cors issue

---
+++ backend/src/main/java/com/microblog/WebConfig.java
@@ -0,0 +1,21 @@
+package com.microblog;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	@Value("${frontend.urls}")
+	private String frontendUrls;
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry
+				.addMapping("/**")
+				.allowedOrigins(frontendUrls.split(","))
+				.allowedMethods("*")
+				.allowCredentials(true);
+	}
+}
