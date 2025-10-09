3340b5530c4f43b1a697150488f07e1f911741f6
ibharathboga <ibharathboga@gmail.com>
2025-10-09 15:24:46 +0000

    [frontend] app.routes, configure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './auth/login.page';
+import { RegisterPage } from './auth/register.page';
+import { ForgotPasswordPage } from './auth/forgot-password.page';
+import { authGuard } from './auth/auth.guard';
+import { WelcomePage } from './welcome.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginPage },
+  { path: 'register', component: RegisterPage },
+  { path: 'forgot-password', component: ForgotPasswordPage },
+  { path: 'welcome', component: WelcomePage, canActivate: [authGuard] },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
