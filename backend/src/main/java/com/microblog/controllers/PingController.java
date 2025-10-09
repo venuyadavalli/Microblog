05c646470df2419cec052363a8d88402a60369b4
ibharathboga <ibharathboga@gmail.com>
2025-10-09 02:03:30 +0000

    [backend] PingController, implement: /ping/db

--- backend/src/main/java/com/microblog/controllers/PingController.java
+++ backend/src/main/java/com/microblog/controllers/PingController.java
@@ -2,15 +2,29 @@ package com.microblog.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/ping")
 public class PingController {
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
 
   @GetMapping
   public String sayPong() {
     return "pong";
+  }
+
+  @GetMapping(value = "/db")
+  public String pingDb() {
+    try {
+      jdbcTemplate.execute("SELECT 1");
+      return "pong (DB OK)";
+    } catch (Exception e) {
+      return "pong (DB FAIL): " + e.getMessage();
+    }
   }
 
 }
