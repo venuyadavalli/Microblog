38d3541aea3208e414746a720154713f97300704
ibharathboga <ibharathboga@gmail.com>
2025-10-10 00:20:35 +0000

    [frontend] auth.interceptor, refactor

--- frontend/src/app/core/auth.interceptor.ts
+++ frontend/src/app/core/auth.interceptor.ts
@@ -1,26 +1,32 @@
-import { Injectable, inject } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable, from } from 'rxjs';
+import { inject } from '@angular/core';
+import { HttpInterceptorFn } from '@angular/common/http';
+import { from } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { Auth } from '@angular/fire/auth';
 
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
-  private auth = inject(Auth);
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const auth = inject(Auth);
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return from(this.auth.currentUser?.getIdToken() ?? Promise.resolve(null)).pipe(
-      switchMap(token => {
-        let authReq = request;
-        if (token) {
-          authReq = request.clone({
-            setHeaders: {
-              Authorization: `Bearer ${token}`
-            }
-          });
-        }
-        return next.handle(authReq);
-      })
-    );
+  function getBearerToken() {
+    const user = auth.currentUser;
+    return from(user ? user.getIdToken() : Promise.resolve(null));
   }
-}
+
+  function addAuthorizationHeader(token: string | null) {
+    return token
+      ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } })
+      : req;
+  }
+
+  // since auth can change at any time
+  // we have to make sure that -during- processing or intercepting request
+  // we are very close to the source of truth
+  // that is to make sure that we are considering the latest state of auth for token
+  // in order to do that whenever auth changes
+  // the token might either get null or refreshed
+  // in such cases switchMap will be required
+  // to make sure to consider latest auth state associated access token
+  return getBearerToken().pipe(
+    switchMap(token => next(addAuthorizationHeader(token)))
+  );
+};
